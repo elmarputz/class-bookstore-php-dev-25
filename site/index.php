e9b878c1712b8ef08ba66afa83ea5cb9a1e4f84a
Elmar Putz <ep@studiomitte.com>
2025-03-15 09:02:54 +0100

    view loader

--- site/index.php
+++ site/index.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>
+<?php
+
+require_once ('inc/bootstrap.php');
+
+$view = $default_view;
+
+if (isset($_REQUEST['view'])
+    && !empty($_REQUEST['view'])
+    && file_exists(__DIR__ . '/views/' . $_REQUEST['view'] . '.php')) {
+
+    $view = $_REQUEST['view'];
+}
+
+require_once ('views/' . $view . '.php');
---
+++ site/views/list.php
@@ -0,0 +1,14 @@
+
+<?php
+require_once "views/partials/header.php";
+?>
+
+
+<div class="page-header">
+    <h2>Book List</h2>
+</div>
+
+
+<?php
+require_once "views/partials/footer.php";
+?>
